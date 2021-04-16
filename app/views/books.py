a01bd9a192e2f5e73abed39cc959f7e19298e1f2
Priscilla-Odame <priscillaodame5@gmail.com>
2021-04-16 11:44:56 +0000

    Implement file upload

--- app/views/books.py
+++ app/views/books.py
@@ -3,6 +3,7 @@ from app.submodels.books import Book
 from app.serializers.books import BooksSerializer
 from rest_framework import viewsets
 from rest_framework import permissions
+from django.http import HttpResponse
 
 
 class BookViews(viewsets.ModelViewSet):
@@ -10,6 +11,34 @@ class BookViews(viewsets.ModelViewSet):
     serializer_class = BooksSerializer
     queryset = Book.objects.all()
     filterset_fields = ['title', 'author']
+
+    def post(self,request):
+        file_model = Book()
+        
+        _, file = request.FILES.popitem() #get first element of uploaded images
+        file = file[0] # get the file from MultiValueDict
+
+        file_model.file = file
+        file_model.save()
+
+        return HttpResponse(content_type='text/plain', content='File added')
+
+# class FileUpload(viewsets.ModelViewSet):
+#     #permission_classes = (IsAdminUser, IsAuthenticated)
+
+#     serializer_class = BooksSerializer
+#     queryset = Book.objects.all()
+
+#     def post(self,request):
+#         file_model = Book()
+        
+#         _, file = request.FILES.popitem() #get first element of uploaded images
+#         file = file[0] # get the file from MultiValueDict
+
+#         file_model.file = file
+#         file_model.save()
+
+#         return HttpResponse(content_type='text/plain', content='File added')
 
 def books(request):
     # all_books = Book.objects.all()
